1a7821b8115be3f063860a60aa169116e394ad95
Nishit Mohanan <nishitmohanan@gmail.com>
2019-02-02 00:35:01 +0530

    store the initial dates required by the api

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import '../css/App.css';
 
 class App extends Component {
+    current_date_obj = new Date();
+    yesterday_date_obj = new Date();
+    yesterday = new Date(new Date().setDate(new Date().getDate() - 1)).getDate();
+    month =this.current_date_obj.getMonth()+1
   state = {
     tendingArticles:[],
-    trendDisplay:[]
+    trendDisplay:[],
+    day:this.yesterday>9?this.yesterday:`0${this.yesterday}`,
+    month:this.month>9?this.month:`0${this.month}`,
+    year:this.current_date_obj.getFullYear()
   };
 
   getTrends = (day,month,year) =>{
+
     fetch('https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia.org/all-access/2019/01/29',
     {
       'headers':
@@ -27,7 +35,8 @@ class App extends Component {
   }
 
   componentDidMount(){
-    this.getTrends(29,'01',2019);
+
+    this.getTrends(this.state.day,this.state.month,this.state.year);
 
   }
 
