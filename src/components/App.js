02ea879766d19718e8f9dd1d5b6ab5674a328492
Nishit Mohanan <nishitmohanan@gmail.com>
2019-02-03 01:18:28 +0530

    added retry logic in case no data is returned by the API

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,73 @@
-import React, { Component } from 'react';
-import '../css/App.css';
+import React, { Component } from "react";
+import "../css/App.css";
 
 class App extends Component {
-    current_date_obj = new Date();
-    yesterday_date_obj = new Date();
-    yesterday = new Date(new Date().setDate(new Date().getDate() - 1)).getDate();
-    month =this.current_date_obj.getMonth()+1
+  current_date_obj = new Date();
+  yesterday_date_obj = new Date();
+  // the API only has the previous days data
+  yesterday = new Date(new Date().setDate(new Date().getDate() - 1)).getDate();
+  // in case previous date has no data
+  day_before_yesterday = new Date(new Date().setDate(new Date().getDate() - 2)).getDate();
+  month = this.current_date_obj.getMonth() + 1;
   state = {
-    tendingArticles:[],
-    trendDisplay:[],
-    day:this.yesterday>9?this.yesterday:`0${this.yesterday}`,
-    month:this.month>9?this.month:`0${this.month}`,
-    year:this.current_date_obj.getFullYear()
+    trendingArticles: [],
+    trendDisplay: [],
+    day: this.yesterday > 9 ? this.yesterday : `0${this.yesterday}`,
+    month: this.month > 9 ? this.month : `0${this.month}`,
+    year: this.current_date_obj.getFullYear()
   };
 
-  getTrends = (day,month,year) =>{
-
-    fetch('https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia.org/all-access/2019/01/29',
-    {
-      'headers':
-        {
-          'Origin': '*',
+  getTrends = (day, month, year) => {
+    const apidata = fetch(
+      `https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia.org/all-access/${year}/${month}/${day}`,
+      {
+        headers: {
+          Origin: "*"
         }
-    })
-    .then(response => {
-      return response.json();
-    }).then(response=>{
-      console.log(response)
-    });
+      }
+    )
+      .then(response => {
+        if (response.status === 404) {
+          throw new Error("No Data Fetched");
+        }
+        return response.json();
+      })
+      .then(response => {
+        console.log(response)
+        this.setState({
+          trendingArticles: response
+        });
+        return response;
+      })
+      .catch(error => {
+        return false;
+      });
+    return apidata;
+  };
 
-    // this.setState({
-    //   tendingArticles : trends
-    // })
+  componentDidMount() {
+    const data = this.getTrends(
+      this.state.day,
+      this.state.month,
+      this.state.year
+    );
+
+    data
+      .then(response => {
+        if (response === false) {
+          throw new Error("No Data Fetched");
+        }
+      })
+      .catch(error => {
+        this.setState({
+          day:this.day_before_yesterday > 9 ? this.day_before_yesterday : `0${this.day_before_yesterday}`
+        })
+        this.getTrends(this.state.day, this.state.month, this.state.year);
+      });
   }
-
-  componentDidMount(){
-
-    this.getTrends(this.state.day,this.state.month,this.state.year);
-
-  }
-
 
   render() {
-    return (
-      <div className="App">
-
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
