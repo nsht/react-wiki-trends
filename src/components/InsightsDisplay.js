eaf5b1d83a56a11b2d34fe21dab31cc290a138d4
Nishit Mohanan <nishitmohanan@gmail.com>
2019-02-09 01:33:54 +0530

    displayed image and created function to fetch thumbnail from object

--- src/components/InsightsDisplay.js
+++ src/components/InsightsDisplay.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import Paper from '@material-ui/core/Paper';
+import React from "react";
+import Paper from "@material-ui/core/Paper";
 
-const InsightsDisplay = (props) => {
+const InsightsDisplay = props => {
+  const getThumbnail = (obj, default_value = "") => {
+    if ('thumbnail' in obj) {
+      return obj['thumbnail'].source;
+    } else {
+      return default_value;
+    }
+  };
   return (
     <>
-    {props.trendDisplay.map((key,index) =>(
-      <Paper className="article-card" key={index}>
-      <span className="rank">#{index+1}</span>
-      <div>{key.displaytitle}</div>
-      </Paper>
-    ))}
+      {props.trendDisplay.map((key, index) => (
+        <Paper className="article-card" key={index}>
+          <span className="rank">#{index + 1}</span>
+          <div>{key.displaytitle}</div>
+          <div className="image-container">
+          <img
+            src={getThumbnail(key)}
+            alt={`${key.displaytitle}`}
+            className="article-image"
+          />
+          </div>
+        </Paper>
+      ))}
     </>
   );
 };
