1f68b5e6e247b1b6e7b9d9af9b450092659f85fc
Nishit Mohanan <nishitmohanan@gmail.com>
2019-02-09 01:01:44 +0530

    Added Api calls to get summary data

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import "../css/App.css";
-import moment from 'moment';
-import InsightsDisplay from './InsightsDisplay';
-import Paper from '@material-ui/core/Paper';
+import moment from "moment";
+import InsightsDisplay from "./InsightsDisplay";
+import Paper from "@material-ui/core/Paper";
 
 class App extends Component {
   // Api only has data of the previous day
-  yesterday = moment().subtract(1,'days').format('YYYY/MM/DD')
+  yesterday = moment()
+    .subtract(1, "days")
+    .format("YYYY/MM/DD");
   state = {
     trendingArticles: [],
     trendDisplay: [],
-    date_str: this.yesterday,
+    date_str: this.yesterday
   };
 
-  getTrends = (date_str) => {
+  getTrends = date_str => {
     const apidata = fetch(
       `https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia.org/all-access/${date_str}`,
       {
@@ -40,10 +42,27 @@ class App extends Component {
     return apidata;
   };
 
+  getSummary = article_slug => {
+    const summary = fetch(
+      `https://en.wikipedia.org/api/rest_v1/page/summary/${article_slug}`
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        console.log(response);
+        const trendDisplay = this.state.trendDisplay;
+        trendDisplay.push(response)
+        this.setState({
+          trendDisplay
+        })
+      });
+    return summary;
+  };
+
+  // https://en.wikipedia.org/api/rest_v1/page/summary/September_11_attacks
   componentDidMount() {
-    const data = this.getTrends(
-      this.state.date_str,
-    );
+    const data = this.getTrends(this.state.date_str);
 
     data
       .then(response => {
@@ -52,11 +71,13 @@ class App extends Component {
         }
       })
       .catch(error => {
-        console.log(error);
+        // console.log(error);
         // change the date to that of day before yesterday in case yesterday's date has no data
         // this could happen if the call is made before the data for the day is updated by wikimedia
         this.setState({
-          date_str: moment().subtract(2,'days').format('YYYY/MM/DD')
+          date_str: moment()
+            .subtract(2, "days")
+            .format("YYYY/MM/DD")
         });
         this.getTrends(this.state.date_str);
       });
@@ -68,9 +89,11 @@ class App extends Component {
     if (articles.length > 0) {
       const filteredArticles = this.filterData(articles);
       if (filteredArticles.length !== articles.length) {
+        let trendDisplay = filteredArticles.slice(0, 10).map(article =>
+          this.getSummary(article.article)
+        );
         this.setState({
           trendingArticles: filteredArticles,
-          trendDisplay: filteredArticles.slice(0, 10)
         });
       }
     }
@@ -96,12 +119,12 @@ class App extends Component {
   };
 
   render() {
-    return(
-      <div className="App" >
-      <h1>WikiPedia Trends</h1>
-      <Paper className="paper-container">
-        <InsightsDisplay trendDisplay={this.state.trendDisplay} />
-      </Paper>
+    return (
+      <div className="App">
+        <h1>WikiPedia Trends</h1>
+        <Paper className="paper-container">
+          <InsightsDisplay trendDisplay={this.state.trendDisplay} />
+        </Paper>
       </div>
     );
   }
