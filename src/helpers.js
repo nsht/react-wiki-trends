d0e441a6f69a8219c40bf60103bc061dc4afbb49
Nishit Mohanan <nishitmohanan@gmail.com>
2019-02-12 00:41:21 +0530

    Added helpers file to seperate logic from react

---
+++ src/helpers.js
@@ -0,0 +1,9 @@
+
+export function getSummary(article_slug){
+    const summary = fetch(
+      `https://en.wikipedia.org/api/rest_v1/page/summary/${article_slug}`
+    ).then(response => {
+      return response.json();
+    });
+    return summary;
+  };
