9fa88bbfee334439d574e1c18065be8d3ba9dfba
Nishit Mohanan <nishitmohanan@gmail.com>
2019-02-15 01:48:18 +0530

    Autoformatted code using prettier

--- src/components/InsightCard.js
+++ src/components/InsightCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
@@ -9,50 +9,53 @@ import Typography from "@material-ui/core/Typography";
 import CardActions from "@material-ui/core/CardActions";
 import Button from "@material-ui/core/Button";
 
+const InsightCard = props => {
+  const getThumbnail = (obj, default_value = "") => {
+    if ("thumbnail" in obj) {
+      return obj["thumbnail"].source;
+    } else {
+      return default_value;
+    }
+  };
 
-const InsightCard = (props) =>{
-
-    const getThumbnail = (obj, default_value = "") => {
-        if ("thumbnail" in obj) {
-          return obj["thumbnail"].source;
-        } else {
-          return default_value;
-        }
-      };
-
-    return(
-        <Card className="article-card" key={props.index}>
-          <a
-            className="article-link"
-            href={props.trendData.content_urls.desktop.page}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <CardHeader
-              avatar={<Avatar aria-label="Rank">{props.index + 1}</Avatar>}
-              title={props.trendData.displaytitle}
-              className="card-header"
-              // subheader={props.trendData.description}
-            />
-          </a>
-          <CardMedia
-            className="card-media"
-            image={getThumbnail(props.trendData)}
-            title={props.trendData.displaytitle}
-          />
-          <CardContent>
-            <Typography component="p">
-              {/* todo truncate */}
-              {props.trendData.description.replace(/^\w/, c => c.toUpperCase())}
-            </Typography>
-          </CardContent>
-          <CardActions className="card-action-container">
-            <Button variant="outlined" size="medium" color="primary" className="trend-button">
-              View Trends
-            </Button>
-          </CardActions>
-        </Card>
-    )
-}
+  return (
+    <Card className="article-card" key={props.index}>
+      <a
+        className="article-link"
+        href={props.trendData.content_urls.desktop.page}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <CardHeader
+          avatar={<Avatar aria-label="Rank">{props.index + 1}</Avatar>}
+          title={props.trendData.displaytitle}
+          className="card-header"
+          // subheader={props.trendData.description}
+        />
+      </a>
+      <CardMedia
+        className="card-media"
+        image={getThumbnail(props.trendData)}
+        title={props.trendData.displaytitle}
+      />
+      <CardContent>
+        <Typography component="p">
+          {/* todo truncate */}
+          {props.trendData.description.replace(/^\w/, c => c.toUpperCase())}
+        </Typography>
+      </CardContent>
+      <CardActions className="card-action-container">
+        <Button
+          variant="outlined"
+          size="medium"
+          color="primary"
+          className="trend-button"
+        >
+          View Trends
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
 
 export default InsightCard;
--- src/components/InsightsDisplay.js
+++ src/components/InsightsDisplay.js
@@ -1,13 +1,12 @@
 import React from "react";
-import InsightCard from './InsightCard';
+import InsightCard from "./InsightCard";
 // import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
 const InsightsDisplay = props => {
-
   return (
     <>
       {props.trendDisplay.map((key, index) => (
-        <InsightCard trendData = {props.trendData[key]} index = {index} ></InsightCard>
+        <InsightCard trendData={props.trendData[key]} index={index} />
       ))}
     </>
   );
